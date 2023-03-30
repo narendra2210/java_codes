73133d5f3b6ade0f269f8025ccdf5c349980b0eb
Narendra Singh <narendra.rocks2002@gmail.com>
2023-03-30 23:10:22 +0530

    Question

---
+++ Super150/Stack_and_Queue/Maximum_Score_of_Good_Subarray.java
@@ -0,0 +1,57 @@
+package Super150.Stack_and_Queue;
+import java.util.*;
+public class Maximum_Score_of_Good_Subarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(Histogram(arr,k));
+    }
+    public static int Histogram(int arr[] , int k){
+        Stack<Integer> stack = new Stack<>();
+        int len =arr.length;
+        int answer = 0;
+        for(int i=0;i<len;i++){
+            while(!stack.isEmpty() && arr[i]<arr[stack.peek()]){
+               int h = arr[stack.pop()];
+               int r = i;
+               if(r-1>=k){
+                if(stack.isEmpty()){
+                    int a = h*r;
+                    answer = Math.max(answer , a);
+                   }
+                   else{
+                    int l =stack.peek();
+                    if(l+1<=k){
+                        int a = h * (r-l-1);
+                        answer = Math.max(answer ,a);
+                    }
+                   }
+               }
+            }
+            stack.push(i);
+        }
+        int r = arr.length;
+        while(!stack.isEmpty()){
+            int h = arr[stack.pop()];
+            if(r-1>=k){
+                if(stack.isEmpty()){
+                    int a = h*r;
+                    answer = Math.max(answer , a);
+                   }
+                   else{
+                    int l =stack.peek();
+                    if(l+1<=k){
+                        int a = h * (r-l-1);
+                        answer = Math.max(answer ,a);
+                    }
+                   }
+               }
+         }
+        return answer;
+    }
+}
