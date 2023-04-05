4cf12a80663c2dbb852e5215331b2cf5709764ad
Narendra Singh <narendra.rocks2002@gmail.com>
2023-04-05 12:14:28 +0530

    Linked List Client

---
+++ Super150/Linked_List/Linked_List_Client.java
@@ -0,0 +1,15 @@
+package Super150.Linked_List;
+
+public class Linked_List_Client {
+    public static void main(String[] args) {
+        Linked_List ll=new Linked_List();
+        ll.Addfirst(10);
+        ll.Addfirst(20);
+        ll.Addfirst(30);
+        ll.Addfirst(40);
+        ll.Display();
+        ll.Addlast(100);
+        ll.Addlast(200);
+        ll.Display();
+    }
+}
