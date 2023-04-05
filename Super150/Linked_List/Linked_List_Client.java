a156c6e6a117326711657d2e1e0fe2fa73722410
Narendra Singh <narendra.rocks2002@gmail.com>
2023-04-05 12:47:33 +0530

    Linked_List_Client

--- Super150/Linked_List/Linked_List_Client.java
+++ Super150/Linked_List/Linked_List_Client.java
@@ -1,7 +1,7 @@
 package Super150.Linked_List;
 
 public class Linked_List_Client {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         Linked_List ll=new Linked_List();
         ll.Addfirst(10);
         ll.Addfirst(20);
@@ -11,5 +11,13 @@ public class Linked_List_Client {
         ll.Addlast(100);
         ll.Addlast(200);
         ll.Display();
+        System.out.println(ll.GetNode(4).val);
+
+        ll.Add_At_Index(10, 2);
+        ll.Display();
+
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+        System.out.println(ll.getatIndex(2));
     }
 }
