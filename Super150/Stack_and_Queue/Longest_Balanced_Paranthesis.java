e9473493336a19daaee685c92f9d6ee6cdbcee6f
Narendra Singh <narendra.rocks2002@gmail.com>
2023-04-24 22:23:26 +0530

    Longest Balanced Paranthesis

--- Super150/Stack_and_Queue/Longest_Balanced_Paranthesis.java
+++ Super150/Stack_and_Queue/Longest_Balanced_Paranthesis.java
@@ -1,3 +1,30 @@
+// Monu bhaiya loves to play with parenthesis.He decides to play a game.
+
+// How long can a balanced expression be? For example, given a string containing
+//  only opening and closing parentheses, what is the maximum length of a well-formed 
+//  parentheses substring? In other words, how many consecutive parentheses can be properly
+//   matched and closed within the string?
+
+// Given a string containing just the characters '(' and ')', print the length of the 
+// longest valid (well-formed) parentheses substring
+
+// Input Format
+// First Line:It consists of a string with "(" or ")"
+
+// Constraints
+// 0 <= s.length <= 3 * 10^4
+// s[i] is '(', or ')'.
+
+// Output Format
+// It prints the longest valid well formed substring.
+
+// Sample Input
+// (()
+// Sample Output
+// 2
+// Explanation
+// The longest valid parentheses substring is "()".
+
 package Super150.Stack_and_Queue;
 import java.util.*;
 public class Longest_Balanced_Paranthesis {
