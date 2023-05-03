190be763644c697d0e3d230f434f1edaf4618862
Narendra Singh <narendra.rocks2002@gmail.com>
2023-05-03 23:25:09 +0530

    Balanced Paranthesis

--- Super150/Stack_and_Queue/Balanced_Paranthesis.java
+++ Super150/Stack_and_Queue/Balanced_Paranthesis.java
@@ -27,8 +27,7 @@ public class Balanced_Paranthesis {
 	
 		Scanner s = new Scanner(System.in);
 		String str = s.next();
-		Balanced_Paranthesis mainobj = new Balanced_Paranthesis();
-		StacksUsingArrays stack = mainobj.new StacksUsingArrays(1000);
+		Stack<Character> stack = new Stack<>();
 		if (isBalanced(str , stack)) {
 			System.out.println("Yes");
 		} else {
@@ -37,7 +36,7 @@ public class Balanced_Paranthesis {
 
 	}
 
-	public static boolean isBalanced(String str, StacksUsingArrays stack) throws Exception {
+	public static boolean isBalanced(String str, Stack<Character> stack) throws Exception {
         int len = str.length();
 		if(len % 2 !=0){
 			return false;
@@ -59,74 +58,4 @@ public class Balanced_Paranthesis {
 		}
 		return stack.isEmpty();
 	}
-
-	private class StacksUsingArrays {
-		private int[] data;
-		private int tos;
-
-		public static final int DEFAULT_CAPACITY = 10;
-
-		public StacksUsingArrays() throws Exception {
-
-			this(DEFAULT_CAPACITY);
-		}
-
-		public StacksUsingArrays(int capacity) throws Exception {
-			if (capacity <= 0) {
-				System.out.println("Invalid Capacity");
-			}
-			this.data = new int[capacity];
-			this.tos = -1;
-		}
-
-		public int size() {
-			return this.tos + 1;
-		}
-
-		public boolean isEmpty() {
-			if (this.size() == 0) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		public void push(int item) throws Exception {
-			if (this.size() == this.data.length) {
-				throw new Exception("Stack is Full");
-			}
-			this.tos++;
-			this.data[this.tos] = item;
-		}
-
-		public int pop() throws Exception {
-			if (this.size() == 0) {
-				throw new Exception("Stack is Empty");
-			}
-			int retVal = this.data[this.tos];
-			this.data[this.tos] = 0;
-			this.tos--;
-			return retVal;
-		}
-
-		public int top() throws Exception {
-			if (this.size() == 0) {
-				throw new Exception("Stack is Empty");
-			}
-			int retVal = this.data[this.tos];
-			return retVal;
-		}
-
-		public void display() throws Exception {
-			if (this.size() == 0) {
-				throw new Exception("Stack is Empty");
-			}
-			for (int i = this.tos; i >= 0; i--) {
-				System.out.println(this.data[i]);
-			}
-
-		}
-
-	}
-
 }
