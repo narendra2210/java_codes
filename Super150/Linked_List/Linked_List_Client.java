2b094b71fcda9e8b00d5adbfece554a6969f770d
Narendra Singh <narendra.rocks2002@gmail.com>
2023-05-22 11:02:46 +0530

    Linked_List_Client

--- Super150/Linked_List/Linked_List_Client.java
+++ Super150/Linked_List/Linked_List_Client.java
@@ -1,23 +1,28 @@
 package Super150.Linked_List;
 
 public class Linked_List_Client {
-    public static void main(String[] args)throws Exception {
-        Linked_List ll=new Linked_List();
-        ll.Addfirst(10);
-        ll.Addfirst(20);
-        ll.Addfirst(30);
-        ll.Addfirst(40);
+    public static void main(String[] args) throws Exception {
+        Linked_List ll = new Linked_List();
+        ll.addfrist(10);
+        ll.addfrist(20);
+        ll.addfrist(30);
         ll.Display();
-        ll.Addlast(100);
-        ll.Addlast(200);
+        ll.addlast(40);
+        ll.addlast(50);
+        ll.addlast(60);
+        ll.addlast(70);
+        ll.addlast(80);
+        // ll.Display();
+        // System.out.println(ll.getfirst());
+        // System.out.println(ll.getlast());
+        // System.out.println(ll.removeFisrt());
+        // ll.Display();
+        // System.out.println(ll.removelast());
         ll.Display();
-        System.out.println(ll.GetNode(4).val);
+        System.out.println(ll.removeatindex(3));
+        ll.Display();
+        ll.addatindex(-90, 7);
+        ll.Display();
 
-        ll.Add_At_Index(10, 2);
-        ll.Display();
-
-        System.out.println(ll.getFirst());
-        System.out.println(ll.getLast());
-        System.out.println(ll.getatIndex(2));
     }
 }
