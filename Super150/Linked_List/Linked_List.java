b124817d73eebce2c0adda59da2b18513618f53d
Narendra Singh <narendra.rocks2002@gmail.com>
2023-05-22 11:00:51 +0530

    Linked_List

--- Super150/Linked_List/Linked_List.java
+++ Super150/Linked_List/Linked_List.java
@@ -1,147 +1,163 @@
 package Super150.Linked_List;
 
 public class Linked_List {
-    public class Node{
-        int val;     //(value of node)
-        Node next;   //(next address)
-    }
-    private Node head; //(first node)
-    private int size; //(total no of nodes)
-    private Node tail; //(last node and after last node there is null)  
+    public class Node {
+        int data;
+        Node next;
 
-    public void Addfirst(int item){
-        Node nn=new Node();
-        nn.val=item; //1,2,3
-        if(size==0){
-            this.head=nn; //1k head
-            this.tail=nn; //1k    //this is a keyword use to access the variable of class not function
-            this.size++;
+        public Node(int data) {
+            this.data = data;
         }
-        else{
-            nn.next=head; //1k  2k
-            this.head=nn; //2k head   3k head
-            this.size++;
-        }
+
     }
 
-    public void Display(){
-        Node temp =head;
-        while(temp!=null){
-            System.out.print(temp.val+" --> ");
-            temp=temp.next;
+    private Node head;
+    private int size;
+    private Node tail;
+
+    public void addfrist(int item) {
+
+        Node nn = new Node(item);
+        if (this.size == 0) {
+            this.head = nn;
+            this.tail = nn;
+            this.size++;
+
+        } else {
+            nn.next = this.head;
+            this.head = nn;
+            this.size++;
+
+        }
+
+    }
+
+    public void addlast(int item) {
+        if (this.size == 0) {
+            addfrist(item);
+        } else {
+            Node nn = new Node(item);
+            this.tail.next = nn;
+            this.tail = nn;
+            this.size++;
+        }
+
+    }
+
+    public void addatindex(int item, int k) throws Exception {
+
+        if (k < 0 || k > size) {
+            throw new Exception("Index out of Bound pgl k ki value range me de");
+        }
+        if (k == 0) {
+
+            addfrist(item);
+        } else if (k == size) {
+            addlast(item);
+        } else {
+            Node nn = new Node(item);
+            Node k_1th = GetNode(k - 1);
+            nn.next = k_1th.next;
+            k_1th.next = nn;
+            this.size++;
+
+        }
+
+    }
+
+    // get
+
+    public int getfirst() throws Exception {
+        if (this.head == null) {
+            throw new Exception("pgl Linkedlsit khaali hai");
+        }
+
+        return this.head.data;
+    }
+
+    public int getlast() throws Exception {
+        if (this.head == null) {
+            throw new Exception("pgl Linkedlsit khaali hai");
+        }
+
+        return this.tail.data;
+    }
+
+    public int getatindex(int k) throws Exception {
+
+        return GetNode(k).data;
+
+    }
+
+    private Node GetNode(int k) throws Exception {
+        if (k < 0 || k >= size) {
+            throw new Exception("Index out of Bound pgl k ki value range me de");
+        }
+        Node temp = head;
+        for (int i = 1; i <= k; i++) {
+            temp = temp.next;
+
+        }
+        return temp;
+
+    }
+
+    public int removeFisrt() throws Exception {
+        if (this.head == null) {
+            throw new Exception("pgl Linkedlsit khaali hai");
+        }
+        Node temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.size--;
+        return temp.data;
+
+    }
+
+    public int removelast() throws Exception {
+        if (this.head == null) {
+            throw new Exception("pgl Linkedlsit khaali hai");
+        }
+        if (this.size == 1) {
+            return removeFisrt();
+        }
+
+        Node temp = GetNode(this.size - 2);// 40
+        int rv = tail.data;// 50
+        this.tail = temp;
+        this.tail.next = null;
+        this.size--;
+        return rv;
+
+    }
+
+    public int removeatindex(int k) throws Exception {
+        if (k < 0 || k >= size) {
+            throw new Exception("Index out of Bound pgl k ki value range me de");
+        }
+
+        if (k == 0) {
+            return removeFisrt();
+        } else if (k == this.size - 1) {
+            return removelast();
+        } else {
+            Node kth = GetNode(k);
+            Node k_1th = GetNode(k - 1);
+            k_1th.next = kth.next;
+            kth.next = null;
+            this.size--;
+            return kth.data;
+
+        }
+
+    }
+
+    public void Display() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
         }
         System.out.println(".");
-    }
-
-    public void Addlast(int item){
-        if(this.size==0){
-            Addfirst(item);
-        }
-        else{
-            Node nn=new Node();
-            nn.val=item;
-            tail.next=nn;
-            tail=nn;
-            size++;
-        }
-    }
-    public Node GetNode(int k)throws Exception{
-         if(k<=0 || k>=size){
-            throw new Exception("Bklol");
-         }
-         Node temp = head;
-         for(int i=0;i<=k;i++){
-            temp = temp.next;
-         }
-         return temp;
-    }
-
-    public void addAtIndex(int item , int k)throws Exception{
-        if(k<=0 || k>=size){
-            throw new Exception("Bklol");
-         }
-         if(k==0){
-            Addfirst(item);
-         }
-         else if(k==size){
-            Addlast(item);
-         }
-         else{
-            Node nn =  new Node();
-            nn.val = item;
-            Node k_1th = GetNode(k-1);
-            Node Kth = k_1th.next;
-            k_1th.next = nn;
-            nn.next = Kth;
-            size++;
-         }
-    }
-
-    public int getFirst(){
-        return head.val;
-    }
-
-    public int getLast(){
-        return tail.val;
-    }
-
-    public int getatIndex(int k)throws Exception{
-        return GetNode(k).val;
-    }
-
-    public int removeFirst()throws Exception{
-        if(size == 0){
-            throw new Exception("Linked list is Empty");
-        }
-        int rv = head.val;
-        if(size == 1){
-           head = null;
-           tail = null;
-           size--;
-        }
-        else{
-            Node temp = head;
-            head = head.next;
-            temp.next = null;
-            size--;
-        }
-        return rv;
-    }
-
-    public int removeLast()throws Exception{
-        if(size == 0){
-            throw new Exception("Linked list is Empty");
-        }
-        int rv = tail.val;
-        if(size == 1){
-          return removeFirst();
-        }
-        else{
-            Node n = GetNode(size-1);
-            tail = n;
-            tail.next = null;
-            size--;
-        }
-        return rv;
-    }
-
-    public int removeAtIndex(int k)throws Exception{
-        if(k < size || k > size){
-            throw new Exception("Empty h bro");
-        }
-        if(k == 0){
-            return removeFirst();
-        }
-        else if(k == size-1){
-            return removeLast();
-        }
-        else{
-            Node K_1th = GetNode(k-1);
-            Node kth = K_1th.next;
-            K_1th.next = kth.next;
-            kth.next = null;
-            size--;
-            return kth.val;
-        }
     }
 }
