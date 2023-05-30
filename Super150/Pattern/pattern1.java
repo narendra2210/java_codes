b528f0a612066844db7eeb77f5cc9a9fdb2edb2a
Narendra Singh <narendra.rocks2002@gmail.com>
2023-05-30 10:32:53 +0530

    pattern1

--- Super150/Pattern/pattern1.java
+++ Super150/Pattern/pattern1.java
@@ -1,19 +1,26 @@
 package Super150.Pattern;
+
 import java.util.Scanner;
 
 public class pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int i = 1;
-        while(n >= i){
-            int a = 1;
-            while(n >= a){
-                System.out.print("* ");
-                a++;
+        int row = 1;
+        int star = n;// number of star in each row
+        while (row <= n) {
+
+            // Start print
+
+            int i = 1;// count of star
+            while (i <= star) {
+                System.out.print("*" + " ");
+                i = i + 1;
             }
-            i++;
+            // next Row Prep
+            row = row + 1;
             System.out.println();
+
         }
     }
 }
