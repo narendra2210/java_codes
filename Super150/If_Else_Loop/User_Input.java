8803d9729742ec04aaa50ddff6631e86e1d6f335
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:33:12 +0530

    User_Input

---
+++ Super150/If_Else_Loop/User_Input.java
@@ -0,0 +1,16 @@
+package Super150.If_Else_Loop;
+
+import java.util.Scanner;
+
+public class User_Input {
+    public static void main(String[] args) {
+
+        // int n=90;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(n);
+
+    }
+}
