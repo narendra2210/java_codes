c4e6df6e4d379a662ec0246b0224ee5b4001b369
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:32:21 +0530

    Simple_Interest

---
+++ Super150/If_Else_Loop/Simple_Interest.java
@@ -0,0 +1,13 @@
+package Super150.If_Else_Loop;
+
+public class Simple_Interest {
+    public static void main(String[] args) {
+		
+		int p = 1000;
+		int r = 5;
+		int t = 2;
+		int si = (p * r * t) / 100;
+		System.out.println(si);
+
+	}
+}
