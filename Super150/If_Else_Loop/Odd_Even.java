37e5d4fad27fb5c7290afddaa1457238a485b080
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:31:19 +0530

    Odd Even

---
+++ Super150/If_Else_Loop/Odd_Even.java
@@ -0,0 +1,18 @@
+package Super150.If_Else_Loop;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+	
+		int n = 894;
+		if (n % 2 == 0) {
+
+			System.out.println("Even Number");
+
+		}
+
+		else {
+			System.out.println("Odd Number");
+		}
+
+	}
+}
