0f4e25867a93538e80415312dcff35ee209e10ef
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:27:43 +0530

    Assignment Operator

--- Super150/If_Else_Loop/Assignment_operator.java
+++ Super150/If_Else_Loop/Assignment_operator.java
@@ -2,7 +2,6 @@ package Super150.If_Else_Loop;
 
 public class Assignment_operator {
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int x=5;
 		x -=5;// x = x-5;
 		System.out.println(x);
