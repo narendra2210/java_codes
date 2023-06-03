37825858eb2a3f95e82f39f91ae08992ea04f89a
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:30:31 +0530

    Maximum_Of_Three

---
+++ Super150/If_Else_Loop/Maximum_of_Three.java
@@ -0,0 +1,20 @@
+package Super150.If_Else_Loop;
+
+public class Maximum_of_Three {
+    public static void main(String[] args) {
+	
+		int a = 18;
+		int b = 18;
+		int c = 8;
+		if (a >= b && a >= c) {
+			System.out.println(a);
+		}
+
+		else if (b > c && b > a) {
+			System.out.println(b);
+		} else {
+			System.out.println(c);
+		}
+
+	}
+}
