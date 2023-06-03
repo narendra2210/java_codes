a243810258c7f74e1e52eaf11d0b3b017cc39530
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-03 11:29:32 +0530

    Loop demo

---
+++ Super150/If_Else_Loop/Loop_demo.java
@@ -0,0 +1,19 @@
+package Super150.If_Else_Loop;
+
+public class Loop_demo {
+    public static void main(String[] args) {
+		
+//		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		System.out.println("Hello World");
+//		System.out.println("Hello World");
+		int i = 1;
+		while (i <= 5) {
+			System.out.println("Hello World");
+			i = i + 1;
+		}
+
+	}
+}
