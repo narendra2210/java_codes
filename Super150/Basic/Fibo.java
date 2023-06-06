93b72bb671ea4370de297ecf62f131eeca8fceaa
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-06 10:27:51 +0530

    Fibonacci

---
+++ Super150/Basic/Fibo.java
@@ -0,0 +1,22 @@
+package Super150.Basic;
+
+import java.util.Scanner;
+
+public class Fibo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+
+        }
+
+        System.out.println(a);
+
+    }
+}
