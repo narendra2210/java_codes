889c154b3120f58b7d7b0dea9f473a3071271a11
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-07 11:29:57 +0530

    Pre_Inc_Dec

---
+++ Super150/Basic/Pre_Inc_Dec.java
@@ -0,0 +1,17 @@
+public class Pre_Inc_Dec {
+    public static void main(String[] args) {
+
+        int a = 7;
+        ++a;// a=a+1;
+        // --a;// a=a-1;
+        // System.out.println(a);
+        // System.out.println(--a);//Pre dec
+
+        // int b= a++ +9 -a - --a;
+        // System.out.println(b);
+
+        // int b = a++ + 7 + a + a-- + 3;
+        // System.out.println(b);
+
+    }
+}
