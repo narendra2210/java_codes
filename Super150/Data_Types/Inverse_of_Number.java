dd6e126856abd94db34290a391da8ee5e52b25b1
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-15 11:46:01 +0530

    Inverse_of_Number

---
+++ Super150/Data_Types/Inverse_of_Number.java
@@ -0,0 +1,16 @@
+public class Inverse_of_Number {
+    public static void main(String[] args) {
+
+        int n = 32145;
+        int pos = 1;
+        int ans = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            ans = (int) (ans + pos * (Math.pow(10, rem - 1)));
+            n /= 10;
+            pos++;
+        }
+        System.out.println(ans);
+
+    }
+}
