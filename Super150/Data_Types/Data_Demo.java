cbb91c64f64ae1e7919e3262211f05057f971b4f
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-15 11:48:18 +0530

    Data_Demo

---
+++ Super150/Data_Types/Data_Demo.java
@@ -0,0 +1,27 @@
+public class Data_Demo {
+    public static void main(String[] args) {
+
+        byte b = 14;
+        System.out.println(b);
+        int i = 428;
+        b = (byte) i;
+        b++;// b= (byte)(b+1)
+        System.out.println(b);
+
+        int ii = 178;
+        byte b1 = -5;
+        ii = b1;
+        System.out.println(ii);
+
+        long ll = 27168288800L;
+        System.out.println(ll);
+
+        float f = 7.91f;// 32
+        double d = 67.9;// 64bit
+        boolean bt = true;
+        boolean bt1 = false;
+        b = (byte) 128;
+        System.out.println(b);
+
+    }
+}
