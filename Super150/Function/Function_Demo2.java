10e48f1b8c1bdf3403ec8d3800132b020423e71f
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-20 16:52:52 +0530

    Function_Demo2

---
+++ Super150/Function/Function_Demo2.java
@@ -0,0 +1,19 @@
+package Super150.Function;
+
+public class Function_Demo2 {
+    public static void main(String[] args) {
+
+        System.out.println("hey");
+        int a = 9;
+        int b = 4;
+        add(a, b);
+        System.out.println("bye");
+
+    }
+
+    public static void add(int a, int b) {
+
+        int c = a + b;
+        System.out.println(c);
+    }
+}
