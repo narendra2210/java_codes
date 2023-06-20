1d2976d473f6307f3a41430fe57c1cc728f49d70
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-20 16:50:55 +0530

    Function Demo

---
+++ Super150/Function/Function_Demo.java
@@ -0,0 +1,29 @@
+package Super150.Function;
+
+public class Function_Demo {
+    public static void main(String[] args) {
+
+        System.out.println("hey");
+        addtwonumber();
+        Subtwonumber();
+        System.out.println("bye");
+
+    }
+
+    public static void addtwonumber() {
+        int a = 9;
+        int b = 11;
+        int c = a + b;
+        Subtwonumber();
+        System.out.println(c);
+
+    }
+
+    public static void Subtwonumber() {
+        int a = 9;
+        int b = 11;
+        int c = a - b;
+        System.out.println(c);
+
+    }
+}
