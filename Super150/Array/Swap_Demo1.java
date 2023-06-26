dd08105aceb3dfde6f86721dd0c7e5200f755062
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-26 11:32:59 +0530

    Swap_Demo1

---
+++ Super150/Array/Swap_Demo1.java
@@ -0,0 +1,21 @@
+package Super150.Array;
+
+public class Swap_Demo1 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int[] arr = { 10, 20, 30, 40, 50 };
+        // int[] other = arr;
+
+        System.out.println(arr[0] + " " + arr[1]);// 10 20
+        swap(arr[0], arr[1]);
+        System.out.println(arr[0] + " " + arr[1]);//
+
+    }
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
