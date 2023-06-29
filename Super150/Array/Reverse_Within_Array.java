a615a94c6345410cdc7ebdeef9b777c7e5dd1fe7
Narendra Singh <narendra.rocks2002@gmail.com>
2023-06-29 16:39:41 +0530

    Reverse_Within_Array

---
+++ Super150/Array/Reverse_Within_Array.java
@@ -0,0 +1,27 @@
+package Super150.Array;
+
+public class Reverse_Within_Array {
+    public static void main(String[] args) {
+
+        int[] arr = { 23, 4, 5, 67, 89, 10, 12 };
+        ReverseArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    public static void ReverseArray(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+
+        }
+
+    }
+}
