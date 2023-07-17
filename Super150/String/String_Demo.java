1308ee0a1b682e0506793de70a9b84906f95ea6f
Narendra Singh <narendra.rocks2002@gmail.com>
2023-07-17 22:51:00 +0530

    String_Demo

---
+++ Super150/String/String_Demo.java
@@ -0,0 +1,16 @@
+package Super150.String;
+
+public class String_Demo {
+    public static void main(String[] args) {
+
+        String str = "hello";
+        String str1 = "hello";
+        System.out.println(str);
+        String st = new String("hello");
+        String st1 = new String("hello");
+        str = str + "bye";
+        st1 = st1 + "bye";
+        System.out.println(str.length());
+
+    }
+}
