04895e42991f8aa43a25ce5653e153dc078679ff
Narendra Singh <narendra.rocks2002@gmail.com>
2023-08-01 11:59:26 +0530

    Coin_Combination

---
+++ Super150/Recursion/Coin_Combination.java
@@ -0,0 +1,25 @@
+package Super150.Recursion;
+
+public class Coin_Combination {
+    public static void main(String[] args) {
+		
+		int[] coin = { 2, 3, 5, 6 };
+		int amount = 10;
+		printCombintation(coin, amount, "", 0);
+
+	}
+
+	public static void printCombintation(int[] coin, int amount, String ans, int idx) {
+		if (amount == 0) {
+			System.out.println(ans);
+			return;
+		}
+		for (int i = idx; i < coin.length; i++) {
+			if (amount >= coin[i]) {
+				printCombintation(coin, amount - coin[i], ans + coin[i], i);
+			}
+
+		}
+
+	}
+}
