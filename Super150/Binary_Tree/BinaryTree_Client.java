fc5a6fd15e301d6ed7fadde4ac40325b59bdd6bb
Narendra Singh <narendra.rocks2002@gmail.com>
2023-08-09 12:12:07 +0530

    BinaryTree_Client

---
+++ Super150/Binary_Tree/BinaryTree_Client.java
@@ -0,0 +1,8 @@
+package Super150.Binary_Tree;
+
+public class BinaryTree_Client {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.Display(nn);
+    }
+}
