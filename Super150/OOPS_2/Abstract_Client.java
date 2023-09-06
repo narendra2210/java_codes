281eb4551aaa8a4337027bfd7b93013bf1473e9f
Narendra Singh <narendra.rocks2002@gmail.com>
2023-09-06 23:48:22 +0530

    OOPS_2

---
+++ Super150/OOPS_2/Abstract_Client.java
@@ -0,0 +1,9 @@
+public class Abstract_Client {
+    public static void main(String[] args) {
+        Abstract_Demo ab = new Abstract_Demo();
+        //As there is no body any of the function so to call the function without body can,t be done
+
+        
+
+    }
+}
---
+++ Super150/OOPS_2/Abstract_Demo.java
@@ -0,0 +1,10 @@
+public abstract class Abstract_Demo {
+
+    // It is not mandatory that all the function in abstract class shuld be abstract
+    public abstract void payment();
+
+    //Abstract method are method which are not given the body
+    public void fun(){
+        
+    }
+}
